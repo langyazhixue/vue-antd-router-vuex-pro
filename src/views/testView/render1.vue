8e5f914358e80031331dc1e6a613add2cbf8d2c3
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-06-05 13:37:24 +0800

    update

---
+++ src/views/testView/render1.vue
@@ -0,0 +1,56 @@
+
+<script>
+export default {
+  name: 'RenderText',
+  props: {
+    level: {
+      type: Number,
+      default: 1
+    }
+  },
+  methods: {
+    alert() {
+      console.log('fsdfd')
+    }
+  },
+  render: function(createElement) {
+    return createElement(
+      // 'h' + this.level, // 标签名称
+      'input',
+      {
+        'class': {
+          foo: true,
+          bar: false
+        },
+        // 与 `v-bind:style` 的 API 相同，
+        // 接受一个字符串、对象，或对象组成的数组
+        style: {
+          color: 'red',
+          fontSize: '14px'
+        },
+        // 普通的 HTML 特性
+        attrs: {
+          id: 'foo',
+          name: 'test'
+        },
+        props: {
+          myProp: 'bar'
+        },
+        // DOM 属性
+        domProps: {
+          value: this.level,
+          checked: true,
+          type: 'radio'
+          // innerHTML: 'baz'
+        },
+        on: {
+          // click: this.alert,
+          change: this.alert
+        }
+      },
+      // this.$slots.default // 子节点数组
+
+    )
+  }
+}
+</script>
