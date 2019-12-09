239db013a15093d6c27bbf898a50eb78301430d2
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-12-09 20:45:09 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,3 @@ module.exports = {
   ],
   'plugins': ['@babel/plugin-syntax-dynamic-import']
 }
-
-// "presets": [["@babel/preset-env",{
-//   "modules": false,
-//   "targets":{
-//     "browsers": ["> 1%", "last 2 versions", "not ie <= 8"],
-//     "node": "current"
-//   },
-// }]],
