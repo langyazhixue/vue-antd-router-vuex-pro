d384f098e0c3b0caa0b0eaf3a2ee2adeaccdc846
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-12-09 20:43:15 +0800

    vue-cli 3.x

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  'plugins': ['@babel/plugin-syntax-dynamic-import']
 }
+
+// "presets": [["@babel/preset-env",{
+//   "modules": false,
+//   "targets":{
+//     "browsers": ["> 1%", "last 2 versions", "not ie <= 8"],
+//     "node": "current"
+//   },
+// }]],
